ea82dc7c36758b0d2cdd18ffb1c67f7e8c8f3da0
NocTempre <158857236+NocTempre@users.noreply.github.com>
2026-02-10 20:28:50 -0500

    Delete legacy code

--- acks-tests/quench-init.js
+++
@@ -1,5 +0,0 @@
-import { exampleUnitTests } from './example-unit-tests.js';
-
-Hooks.once('quenchReady', (quench) => {
-  quench.registerBatch('acks.examples', exampleUnitTests, { displayName: 'Example Unit Tests' });
-});
